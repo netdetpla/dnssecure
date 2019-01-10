f392fa77010b3035275d05c1952acaeb42a4cfbf
SinLapis <stradust0001@gmail.com>
2019-01-10 15:42:00 +0800

    20190110 3

--- dnssecure/src/Main.go
+++ dnssecure/src/Main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-	//"github.com/pkg/sftp"
-	//"log"
+	"github.com/pkg/sftp"
+	"log"
+	"net"
 	"os"
-	//"path"
+	"path"
 	"strconv"
 	"time"
 )
@@ -78,79 +79,79 @@ func main() {
 	_ = SendUDP(tasks.taskID, tasks.subID, "len: "+strconv.Itoa(len(tasks.records))+"; duration: "+strconv.FormatInt(duration, 10))
 	//写状态文件
 	var (
-	//	//err        error
-	//	sftpClient *sftp.Client
-	//)
-	//
-	//// 这里换成实际的 SSH 连接的 用户名，密码，主机名或IP，SSH端口
-	//sftpClient, err = connect("root", "root111111", "10.96.129.6", 22)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//defer sftpClient.Close()
-	//
-	//// 用来测试的本地文件路径 和 远程机器上的文件夹
-	//var localFilePath = "/tmp/result/" + tasks.taskID + ".result"
-	//var remoteDir = "/home/result/"
-	//srcFile, err := os.Open(localFilePath)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//defer srcFile.Close()
-	//now := strconv.FormatInt(time.Now().Unix(), 10)
-	//var remoteFileName = randString + "-" + now //path.Base(localFilePath)
-	//dstFile, err := sftpClient.Create(path.Join(remoteDir, remoteFileName))
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//defer dstFile.Close()
-	//
-	//for {
-	//	buf := make([]byte, 1024)
-	//	n, _ := srcFile.Read(buf)
-	//	if n == 0 {
-	//		break
-	//	}
-	//	dstFile.Write(buf)
-	//}
-	//
-	//fmt.Println("copy file to remote server finished!")
+		//err        error
+		sftpClient *sftp.Client
+	)
+
+	// 这里换成实际的 SSH 连接的 用户名，密码，主机名或IP，SSH端口
+	sftpClient, err = connect("root", "root111111", "10.96.129.6", 22)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sftpClient.Close()
+
+	// 用来测试的本地文件路径 和 远程机器上的文件夹
+	var localFilePath = "/tmp/result/" + tasks.taskID + ".result"
+	var remoteDir = "/home/result/"
+	srcFile, err := os.Open(localFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer srcFile.Close()
+	now := strconv.FormatInt(time.Now().Unix(), 10)
+	var remoteFileName = randString + "-" + now //path.Base(localFilePath)
+	dstFile, err := sftpClient.Create(path.Join(remoteDir, remoteFileName))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dstFile.Close()
+
+	for {
+		buf := make([]byte, 1024)
+		n, _ := srcFile.Read(buf)
+		if n == 0 {
+			break
+		}
+		dstFile.Write(buf)
+	}
+
+	fmt.Println("copy file to remote server finished!")
 	WriteSuccess2Appstatus()
 }
 
-//func connect(user, password, host string, port int) (*sftp.Client, error) {
-//  var (
-//    auth         []ssh.AuthMethod
-//    addr         string
-//    clientConfig *ssh.ClientConfig
-//    sshClient    *ssh.Client
-//    sftpClient   *sftp.Client
-//    err          error
-//  )
-//  // get auth method
-//  auth = make([]ssh.AuthMethod, 0)
-//  auth = append(auth, ssh.Password(password))
-//
-//  clientConfig = &ssh.ClientConfig{
-//    User:    user,
-//    Auth:    auth,
-//    Timeout: 30 * time.Second,
-//	HostKeyCallback:func(hostname string,remote net.Addr,key ssh.PublicKey) error {
-//		return nil
-//	},
-//  }
-//
-//  // connet to ssh
-//  addr = fmt.Sprintf("%s:%d", host, port)
-//
-//  if sshClient, err = ssh.Dial("tcp", addr, clientConfig); err != nil {
-//    return nil, err
-//  }
-//
-//  // create sftp client
-//  if sftpClient, err = sftp.NewClient(sshClient); err != nil {
-//    return nil, err
-//  }
-//
-//  return sftpClient, nil
-//}
+func connect(user, password, host string, port int) (*sftp.Client, error) {
+ var (
+   auth         []ssh.AuthMethod
+   addr         string
+   clientConfig *ssh.ClientConfig
+   sshClient    *ssh.Client
+   sftpClient   *sftp.Client
+   err          error
+ )
+ // get auth method
+ auth = make([]ssh.AuthMethod, 0)
+ auth = append(auth, ssh.Password(password))
+
+ clientConfig = &ssh.ClientConfig{
+   User:    user,
+   Auth:    auth,
+   Timeout: 30 * time.Second,
+	HostKeyCallback:func(hostname string,remote net.Addr,key ssh.PublicKey) error {
+		return nil
+	},
+ }
+
+ // connet to ssh
+ addr = fmt.Sprintf("%s:%d", host, port)
+
+ if sshClient, err = ssh.Dial("tcp", addr, clientConfig); err != nil {
+   return nil, err
+ }
+
+ // create sftp client
+ if sftpClient, err = sftp.NewClient(sshClient); err != nil {
+   return nil, err
+ }
+
+ return sftpClient, nil
+}
